def702cbed2855a6a8e93d707c46a13af07944b7
IT24100796 <it24100796@my.sliit.lk>
2025-09-21 15:46:40 +0530

    commit changes

--- Lab Sheet 06/IT24100796_Lab06.R
+++ Lab Sheet 06/IT24100796_Lab06.R
@@ -8,7 +8,7 @@ getwd()
 
 #Part 2
 #What is the probability that at least 47 students passed the test?
-1 - pbinom(46,50,0.85)
+1 - pbinom(46,50,0.85, lower.tai=TRUE)
 
 #Question 2
 #Part 1
@@ -23,4 +23,5 @@ getwd()
 #What is the probability that exactly 15 calls are received in an hour?
 
 dpois(15,12)
+
 
